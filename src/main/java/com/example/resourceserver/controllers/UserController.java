09cd42f8fbb5e3d75e24fbd108d48fac6ca0de69
eermakov <EVErmakov@lanit.ru>
2023-01-30 20:40:18 +0300

    secured-annotation: add secured method

--- src/main/java/com/example/resourceserver/controllers/UserController.java
+++ src/main/java/com/example/resourceserver/controllers/UserController.java
@@ -1,6 +1,8 @@
 package com.example.resourceserver.controllers;
 
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +14,17 @@ public class UserController {
     public String getMessage() {
         // узнать возможности -> http://localhost:8180/realms/second/.well-known/openid-configuration
         return "YES, I still love you!";
+    }
+
+    @Secured("ROLE_developer")
+    @GetMapping("/secured")
+    public String getMethodLevelSecurityMessage() {
+        return "Method-Level-Security-Message";
+    }
+
+    @Secured("ROLE_user")
+    @GetMapping("/secured-forbidden")
+    public String getMethodLevelSecurityForbiddenMessage() {
+        return "Ты этого не получишь =)";
     }
 }
